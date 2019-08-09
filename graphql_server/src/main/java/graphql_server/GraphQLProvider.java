c9181dafcdac27124d9557104e5d1932aa7146f4
Thijs Wouters <thijs.wouters@cegeka.com>
2019-08-09 14:28:29 +0200

    Can get something

---
+++ graphql_server/src/main/java/graphql_server/GraphQLProvider.java
@@ -0,0 +1,48 @@
+package graphql_server;
+
+import com.google.common.base.Charsets;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.io.Resources;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.URL;
+
+@Component
+public class GraphQLProvider {
+    private GraphQL graphQL;
+
+    @Bean
+    public GraphQL graphQL() {
+        return graphQL;
+    }
+
+    @PostConstruct
+    public void init() throws IOException {
+        URL url = Resources.getResource("schema.graphqls");
+        String sdl = Resources.toString(url, Charsets.UTF_8);
+        GraphQLSchema graphQLSchema = buildSchema(sdl);
+        this.graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+    }
+
+    private GraphQLSchema buildSchema(String sdl) {
+        TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(sdl);
+        RuntimeWiring wiring = buildWiring();
+        return new SchemaGenerator().makeExecutableSchema(typeRegistry, wiring);
+    }
+
+    private RuntimeWiring buildWiring() {
+        return RuntimeWiring.newRuntimeWiring()
+                .type(TypeRuntimeWiring.newTypeWiring("Query")
+                        .dataFetcher("user", environment -> ImmutableMap.of(
+                                "id", "12",
+                                "name", "Thijs",
+                                "age", 37)))
+                .build();
+    }
+}
